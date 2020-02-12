c3a503b00514e84e75fcf567c873a49b98042715
Alexander Kung <akung@secretmission.io>
2020-02-12 16:02:10 -0500

    fix: fixed error handling on `call` method of `APIClient`

--- packages/api/src/APIClient.ts
+++ packages/api/src/APIClient.ts
@@ -104,19 +104,21 @@ export default class APIClient {
 				ok: true,
 				data: response.data,
 			}
-		} catch ({
-			response: {
-				status,
-				statusText,
-				data: { code, message, errors },
-			},
-		}) {
-			throw new APIClientError(
-				name || '',
-				message || statusText,
-				(code && code.toString()) || (status && status.toString()),
-				errors
-			)
+		} catch (err) {
+			if (err.response) {
+				const {
+					response: { status, statusText, data: errData },
+				} = err
+
+				throw new APIClientError(
+					name || '',
+					(errData && errData.message) || statusText,
+					(errData && errData.code && errData.code.toString()) ||
+						(status && status.toString()),
+					errData && errData.errors
+				)
+			}
+			throw err
 		}
 	}
 
